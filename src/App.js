56dd2012bca115bd3f7fc4b897595f16bbf0b130
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2023-01-07 03:28:44 +0330

    Add PostForm component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
 
 function App() {
   return (
     <div className="App">
+      <PostForm />
       <PostList />
     </div>
   );
