66e8b5f32b96d692790b78a9b3c406eb44664661
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2023-01-10 11:53:55 +0330

    Upgrade PostList from old class component to function component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,40 +1,30 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-class PostList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [],
-      errorMessage: "",
-    };
-  }
+function PostList() {
+  const [posts, setPosts] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/posts")
       .then((response) => {
-        this.setState({ posts: response.data });
+        setPosts(response.data);
       })
       .catch((error) => {
-        this.setState({ errorMessage: "Error retreiving data" });
+        console.log("Error retreiving data");
       });
-  }
+  }, []);
 
-  render() {
-    const { posts, errorMessage } = this.state;
-    return (
-      <div>
-        <ul>
-          <h2>List of posts</h2>
-          {posts.length
-            ? posts.map((post) => <li key={post.id}>{post.title}</li>)
-            : null}
-          {errorMessage ? <div>{errorMessage}</div> : null}
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <ul>
+        <h2>List of posts</h2>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default PostList;
